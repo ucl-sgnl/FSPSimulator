66cb016d9d04e8f65a00d9f919f66a09bd7156c8
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-04-18 14:58:57 +0100

    start unit tests and add init.py

--- src/utils/UpdateCatalogue.py
+++ src/utils/UpdateCatalogue.py
@@ -1,3 +1,6 @@
+def update_catalogue_celestrak():
+    print('Updating celestrak files')
+
 import os
 import datetime
 import requests
@@ -13,8 +16,7 @@ def read_csv(file_path):
     return df
 
 def jsr_download_if_newer(local_path, url):
-    # function to download a file from a url if the local file is older than the remote file
-    # avoids having to re-download the file unnecessarily
+    """Download a file from a URL if it is newer than the local file."""
     if os.path.exists(local_path):
         local_last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(local_path))
 
@@ -22,29 +24,22 @@ def jsr_download_if_newer(local_path, url):
         remote_last_modified = datetime.datetime.strptime(response.headers['Last-Modified'], '%a, %d %b %Y %H:%M:%S %Z')
 
         if remote_last_modified <= local_last_modified:
-            print(f"{local_path} is already up to date.")
             return
 
     wget.download(url, local_path)
-    print(f"{local_path} has been updated.")
 
 def update_catalogue_jsr():
-    # set paths
+    """Update the JSR catalogue files."""
     cwd = os.getcwd()
     external_dir = os.path.join(cwd, 'src/data/external/')
     tsv_cat_path = external_dir + 'currentcat.tsv'
     payload_cat_path = external_dir + 'payloadcat.tsv'
 
-    # current cat is the general catalog
-    # payload cat is the payload specific catalog and contains detailed info about payloads (e.g. when they are active)
     urls = {tsv_cat_path: 'https://planet4589.org/space/gcat/tsv/derived/currentcat.tsv',
         payload_cat_path: 'https://planet4589.org/space/gcat/tsv/cat/psatcat.tsv'}
 
     for path, url in urls.items():
         jsr_download_if_newer(path, url)
-
-def update_catalogue_celestrak():
-    print('Updating celestrak files')
 
 if __name__ == '__main__':
     update_catalogue_jsr()
