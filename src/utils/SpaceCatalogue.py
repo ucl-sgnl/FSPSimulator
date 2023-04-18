d5bdda0a7e3f5f311cd55dff45d942b7a1440846
blueriver212 <indy.brownhall@gmail.com>
2023-04-18 15:52:36 +0100

    updating settings env

--- src/utils/SpaceCatalogue.py
+++ src/utils/SpaceCatalogue.py
@@ -1,6 +1,8 @@
 class SpaceCatalogue:
     def __init__(self):
         self.PullCatalogue()
+        self.Satellites = []
+        self.AddSatellites()
 
     def PullCatalogue(self):
         # pull in the latest data based from data/external/catalogue.csv
