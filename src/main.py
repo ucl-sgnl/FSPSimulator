9a3c3833ca10c7a154400a1a00c2f6ba55e395fa
blueriver212 <indy.brownhall@gmail.com>
2023-04-18 14:37:10 +0100

    updating satellite class

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
+from utils.SpaceObject import Satellite
+
 def run_simulation(policy):
     id = policy.get('id')
     print(f'Running simulation for policy {id}')
+
+    # Create a Space Object
+    space_object = Satellite()
+    print(space_object.GUID)
+
+    space_object.cospar_id = '2020-001A'
+
+    print(space_object.cospar_id)
