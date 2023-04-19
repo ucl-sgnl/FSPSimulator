5decb3fe9700ecc5c868c9cdab1a949c36a2cd46
blueriver212 <indy.brownhall@gmail.com>
2023-04-19 17:46:49 +0100

    Completed the extraction of tles, just need to merge them to JSR's catalogue

--- src/utils/SpaceCatalogue.py
+++ src/utils/SpaceCatalogue.py
@@ -1,5 +1,6 @@
 # from SpaceObject import Satellite
-from UpdateCatalogue import update_catalogue_jsr, update_catalogue_celestrak, create_merged_catalogue
+from UpdateCatalogue import update_catalogue_jsr, update_catalogue_celestrak
+from coords import tle_parse
 import pandas as pd
 import os
 
@@ -9,29 +10,41 @@ class SpaceCatalogue:
         self.Satellites = []
         self.Catalogue = None
 
-    def PullCatalogue(self):
-        # pull in the latest data based from data/external/catalogue.csv
-        # update the jsr catalogue if it is older than 1 day
-        update_catalogue_jsr()
-        update_catalogue_celestrak()
-        create_merged_space_catalogue()
-
-    def create_merged_space_catalogue():
+    def create_merged_space_catalogue(self):
     # then pull the most recent file form data/external/currentcat.tsv
         jsr_cat = pd.read_csv(os.path.join(os.getcwd(), 'src/data/external/currentcat.tsv'), sep='\t')
         celestrak_cat = pd.read_csv(os.path.join(os.getcwd(), 'src/data/external/celestrak_active.txt'), sep='\n', header=None)
 
         # convert the celestrak 3LE into one string
         with open(os.path.join(os.getcwd(), 'src/data/external/celestrak_active.txt'), 'r') as f:
-            three_line_elements = [line.strip() for line in f.readlines()]
+            three_line_elements = f.readlines()
 
         tles = []
         for i in range(0, len(three_line_elements), 3):
             tles.append(three_line_elements[i:i+3])
 
+        tles_parsed= []
         # read in 3LE correctly
+        for tle in tles:
+            test = ''.join(tle)
+            tles_parsed.append(tle_parse(test))
 
-        # merge the two dataframes
+
+        # first convert the list of tles to a dataframe
+        tles_dict = pd.DataFrame(tles_parsed)
+
+        # then merge the two dataframes
+        self.Catalogue = pd.merge(jsr_cat, tles_dict, left_on='Satcat', right_on='satellite catalog number')
+
+        self.Catalogue.to_csv(os.path.join(os.getcwd(), 'src/data/external/output_test.tsv'), index=False)
+        
+
+
+    # create a dataframe from the 3LE
+    def PullCatalogue(self):
+        update_catalogue_jsr()
+        update_catalogue_celestrak()
+        self.create_merged_space_catalogue()
 
 if __name__ == '__main__':
     catalogue = SpaceCatalogue()
