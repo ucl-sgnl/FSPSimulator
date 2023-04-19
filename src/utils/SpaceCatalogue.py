3ba285955e66e68ef421c55d7a00ad14fc244f52
blueriver212 <indy.brownhall@gmail.com>
2023-04-19 13:31:26 +0100

    updating before merge

--- src/utils/SpaceCatalogue.py
+++ src/utils/SpaceCatalogue.py
@@ -1,5 +1,5 @@
 # from SpaceObject import Satellite
-from UpdateCatalogue import update_catalogue_jsr, update_catalogue_celestrak
+from UpdateCatalogue import update_catalogue_jsr, update_catalogue_celestrak, create_merged_catalogue
 import pandas as pd
 import os
 
@@ -14,8 +14,10 @@ class SpaceCatalogue:
         # update the jsr catalogue if it is older than 1 day
         update_catalogue_jsr()
         update_catalogue_celestrak()
+        create_merged_space_catalogue()
 
-        # then pull the most recent file form data/external/currentcat.tsv
+    def create_merged_space_catalogue():
+    # then pull the most recent file form data/external/currentcat.tsv
         jsr_cat = pd.read_csv(os.path.join(os.getcwd(), 'src/data/external/currentcat.tsv'), sep='\t')
         celestrak_cat = pd.read_csv(os.path.join(os.getcwd(), 'src/data/external/celestrak_active.txt'), sep='\n', header=None)
 
@@ -27,8 +29,10 @@ class SpaceCatalogue:
         for i in range(0, len(three_line_elements), 3):
             tles.append(three_line_elements[i:i+3])
 
-        # then strip the 
-        print(tles[0:10])
+        # read in 3LE correctly
+
+        # merge the two dataframes
+
 if __name__ == '__main__':
     catalogue = SpaceCatalogue()
 
