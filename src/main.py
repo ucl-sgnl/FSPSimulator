25ddc7bc21e9d0db1197c792355faff8d1bf009e
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-06-09 16:54:47 +0100

    split normal and profiling code - Decay worksgit add .

--- src/main.py
+++ src/main.py
@@ -83,36 +83,6 @@ def propagate_satellite(args):
 
     return satellite
 
-import os
-
-def propagate_satellite_profiling(args):
-    satellite, jd_start, jd_stop = args
-
-    # Create a cProfile.Profile object and enable it to start profiling
-    profiler = cProfile.Profile()
-    profiler.enable()
-
-    # Execute the prop_catobject method
-    satellite.prop_catobject(jd_start=jd_start, jd_stop=jd_stop, step_size=20, propagator="RK45")
-
-    # Disable the profiler
-    profiler.disable()
-
-    # Create the directory if it doesn't exist
-    directory = 'src/tests/profiling_results/'
-    os.makedirs(directory, exist_ok=True)
-
-    # Create the file path
-    file_path = os.path.join(directory, f'profiling_{satellite.rso_name}.txt')
-
-    # Save the stats of the profiler into a unique file for each satellite.
-    with open(file_path, 'w') as f:
-        sys.stdout = f  # redirect output to file
-        profiler.print_stats(sort='time')
-        sys.stdout = sys.__stdout__  # reset output to normal
-
-    return satellite
-
 def run_parallel_sim(settings):
     SATCAT = SpaceCatalogue(settings["sim_object_type"], settings["sim_object_catalogue"], settings["repull_catalogues"])
     jd_start = float(utc_to_jd(settings["sim_start_date"])[0])
@@ -157,7 +127,7 @@ def run_parallel_sim(settings):
     SATCAT.Catalogue = SATCAT.Catalogue[:100]
 
     chunksize = len(SATCAT.Catalogue) // cpu_count()
-    SATCAT.Catalogue = process_map(propagate_satellite_profiling, [(satellite, jd_start, jd_stop) for satellite in SATCAT.Catalogue], max_workers=cpu_count(), chunksize=chunksize)
+    SATCAT.Catalogue = process_map(propagate_satellite, [(satellite, jd_start, jd_stop) for satellite in SATCAT.Catalogue], max_workers=cpu_count(), chunksize=chunksize)
 
     print("Exporting results...")
     dump_pickle(f'src/data/catalogue/prop_{scenario_name}.pickle', SATCAT)
@@ -168,12 +138,5 @@ def run_parallel_sim(settings):
 
 if __name__ == '__main__':
     ##### Profiling #####
-    with open('src/tests/profiling_results/profiling_main.txt', 'w') as f:
-        sys.stdout = f  # redirect output to file
-        policy = json.load(open(get_path('src/data/prediction_csv/sim_settings.json'), 'r'))
-        profiler = cProfile.Profile()
-        profiler.enable()
-        run_parallel_sim(policy)
-        profiler.disable()
-        profiler.print_stats(sort='time')
-    sys.stdout = sys.__stdout__  # reset output to normal
+    policy = json.load(open(get_path('src/data/prediction_csv/sim_settings.json'), 'r'))
+    run_parallel_sim(policy)
