a78138824128b894eb42b7701df98a58f5af94d5
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-07 15:57:23 +0100

    try gcrf

--- src/utils/Conversions.py
+++ src/utils/Conversions.py
@@ -605,7 +605,7 @@ def create_spacecraft_states(positions: List[List[float]], velocities: List[List
     Returns:
         ArrayList: List of SpacecraftState objects.
     """
-    frame = FramesFactory.getTEME()
+    frame = FramesFactory.getGCRF()
     spacecraft_states = ArrayList()
     dates_orekit = [datetime_to_absolutedate(mjd_to_datetime(mjd)) for mjd in dates_mjd]
     for position, velocity, date_orekit in zip(positions, velocities, dates_orekit):
