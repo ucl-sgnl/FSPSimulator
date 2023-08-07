588a5a71cb9b5a1af38ad7f52a690cbbf4297b71
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-07 16:11:28 +0100

    put back to ICRF

--- src/utils/Conversions.py
+++ src/utils/Conversions.py
@@ -606,7 +606,7 @@ def create_spacecraft_states(positions: List[List[float]], velocities: List[List
     Returns:
         ArrayList: List of SpacecraftState objects.
     """
-    frame = FramesFactory.getITRF(IERSConventions.IERS_2010, False)
+    frame = FramesFactory.getICRF()
     spacecraft_states = ArrayList()
     dates_orekit = [datetime_to_absolutedate(mjd_to_datetime(mjd)) for mjd in dates_mjd]
     for position, velocity, date_orekit in zip(positions, velocities, dates_orekit):
