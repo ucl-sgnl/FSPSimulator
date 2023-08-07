d396ac862f0aef46f8c888c0790f158bb7e6196a
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-07 11:38:05 +0100

    mean to true pos angle

--- src/utils/Conversions.py
+++ src/utils/Conversions.py
@@ -671,9 +671,9 @@ def fit_tle_to_spacecraft_states(spacecraft_states: ArrayList, satellite_number:
                           ma,
                           revolution_number,
                           b_star_first_guess)
-    threshold = 1.0 #distnace threshold in meters between the spacecraft state and the TLE
+    threshold = 10.0 #distnace threshold in meters between the spacecraft state and the TLE
     print("tle_first_guess:", tle_first_guess)
-    tle_builder = TLEPropagatorBuilder(tle_first_guess, PositionAngle.MEAN, 1.0) #the 1000 here is the position scale. i.e. the factor by which the "real" orbital parameters are scaled down to produce normalized parameters.
+    tle_builder = TLEPropagatorBuilder(tle_first_guess, PositionAngle.TRUE, 1.0) #the 1000 here is the position scale. i.e. the factor by which the "real" orbital parameters are scaled down to produce normalized parameters.
     print("tle_builder:", tle_builder)
     fitter = FiniteDifferencePropagatorConverter(tle_builder, threshold, 100000) # the 1000 here is the max number of iterations to reach threshold
     print("fitter:", fitter)
