4a213e633247812fc0f79d3427dc68000ee96039
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-07 12:46:40 +0100

    debug

--- src/utils/Conversions.py
+++ src/utils/Conversions.py
@@ -732,8 +732,8 @@ def fit_TLE_to_ephemeris(positions_eci: List[List[float]], velocities_eci: List[
     obstimes = Time(dates)
     mjds = obstimes.mjd
     # Create spacecraft states
-    positions_eci_meters = [[coord * 1000 for coord in position] for position in positions_eci]
-    velocities_eci_meters = [[velocity * 1000 for velocity in velocities] for velocities in velocities_eci]
+    positions_eci_meters = [[coord for coord in position] for position in positions_eci]
+    velocities_eci_meters = [[velocity for velocity in velocities] for velocities in velocities_eci]
     spacecraft_states = create_spacecraft_states(positions_eci_meters, velocities_eci_meters, mjds)
     mean_motion = float(np.sqrt(orekit_constants.EIGEN5C_EARTH_MU / np.power(a*1000, 3)))
     ## Placeholder parameters.
