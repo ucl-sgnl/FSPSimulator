74954a881618f35f8a2a2449bc6a7a2f560a7f19
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-07 15:40:42 +0100

    reduce thresh again

--- src/utils/Conversions.py
+++ src/utils/Conversions.py
@@ -675,11 +675,11 @@ def fit_tle_to_spacecraft_states(spacecraft_states: ArrayList, satellite_number:
                           ma,
                           revolution_number,
                           b_star_first_guess)
-    threshold = 10000.0 #distance threshold in meters between the propagator and the TLE
+    threshold = 10.0 #distance threshold in meters between the propagator and the TLE
     print("tle_first_guess:", tle_first_guess)
-    tle_builder = TLEPropagatorBuilder(tle_first_guess, PositionAngle.MEAN, 10000.0) #the 1.0 here is the position scale. i.e. the factor by which the "real" orbital parameters are scaled down to produce normalized parameters.
+    tle_builder = TLEPropagatorBuilder(tle_first_guess, PositionAngle.MEAN, 1000.0) #the 1.0 here is the position scale. i.e. the factor by which the "real" orbital parameters are scaled down to produce normalized parameters.
     print("tle_builder:", tle_builder)
-    fitter = FiniteDifferencePropagatorConverter(tle_builder, threshold, 1000000) # the 1000 here is the max number of iterations to reach threshold
+    fitter = FiniteDifferencePropagatorConverter(tle_builder, threshold, 100000) # the 1000 here is the max number of iterations to reach threshold
     print("fitter:", fitter)
     fitter.convert(spacecraft_states, True, 'BSTAR')
     print("spacecraft states converted")
