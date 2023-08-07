fa1afdaea56577141fcb7cd9f96512a64e190d77
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-07 16:02:48 +0100

    ITRF frame

--- src/utils/Conversions.py
+++ src/utils/Conversions.py
@@ -605,7 +605,7 @@ def create_spacecraft_states(positions: List[List[float]], velocities: List[List
     Returns:
         ArrayList: List of SpacecraftState objects.
     """
-    frame = FramesFactory.getGCRF()
+    frame = FramesFactory.getITRF()
     spacecraft_states = ArrayList()
     dates_orekit = [datetime_to_absolutedate(mjd_to_datetime(mjd)) for mjd in dates_mjd]
     for position, velocity, date_orekit in zip(positions, velocities, dates_orekit):
@@ -738,6 +738,7 @@ def fit_TLE_to_ephemeris(positions_eci: List[List[float]], velocities_eci: List[
     positions_eci_meters = [[coord *1000 for coord in position] for position in positions_eci]
     velocities_eci_meters = [[velocity * 1000 for velocity in velocities] for velocities in velocities_eci]
     spacecraft_states = create_spacecraft_states(positions_eci_meters, velocities_eci_meters, mjds)
+    print("s/c states:", spacecraft_states)
     mean_motion = float(np.sqrt(orekit_constants.EIGEN5C_EARTH_MU / np.power(a*1000, 3)))
     ## Placeholder parameters.
     ## TODO: I believe none of these are actually used in the propagtion itself but they are required to make a TLE
