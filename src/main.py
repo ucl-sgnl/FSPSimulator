2caa41ffcd5751102041071dcd8b2cdf466340b3
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-08 15:18:26 +0100

    init orekit in propagate_space_object function

--- src/main.py
+++ src/main.py
@@ -25,6 +25,8 @@ def propagate_space_object(args):
     # Execute the prop_catobject method on the space object
     print(f"Propagating {space_object.rso_name}...")
     try:
+        if long_term_sgp4:
+            initialize_orekit()
         space_object.prop_catobject(jd_start=jd_start, jd_stop=jd_stop, step_size=step_size, output_freq=output_freq, integrator_type=integrator_type, force_model=force_model, long_term_sgp4=long_term_sgp4)
     except Exception as e:
         print(f"An error occurred while propagating {space_object.rso_name}: {e}")
