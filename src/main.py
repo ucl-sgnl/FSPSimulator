8d383e52ecf5679603a22a80b9f100f404f48373
Charles C <66725307+CharlesPlusC@users.noreply.github.com>
2023-08-08 15:22:15 +0100

    process-based parallelization

--- src/main.py
+++ src/main.py
@@ -35,57 +35,7 @@ def propagate_space_object(args):
 
     return space_object
 
-###### MULTIPROCESSING ######
-# def run_parallel_sim(settings):
-#     # This function will define the simulation settings and run the simulation based on the SpaceCatalogue class instantiation.
-#     # The SpaceCatalogue class will create a catalogue(list) of SpaceObjects based on the settings provided.
-#     # This list of SpaceObjects is just metadata (empty ephemerides) until we propagate the SpaceObjects using the prop_catobject method.
-
-#     SATCAT = SpaceCatalogue(settings = settings)
-#     jd_start = float(utc_to_jd(settings["sim_start_date"])[0])
-#     jd_stop = float(utc_to_jd(settings["sim_end_date"])[0])
-#     step_size = int(settings["integrator_step_size"]) # in seconds
-#     output_freq = int(settings["output_frequency"]) # in seconds
-#     scenario_name = str(settings["scenario_name"]) # this will be used to name the output pickle file
-#     integrator_type = str(settings["integrator_type"])  # must be one of "RK45", "RK23", "DOP853", "Radau", "BDF", "LSODA"
-#     sgp4_long_term = bool(settings["sgp4_long_term"]) # boolean, if true, use sgp4 long term propagator
-#     force_model = settings["force_model"] # list of strings, can be  # "all" or "grav_mono" and/or "j2"  and/or "sun_grav" and/or "moon_grav" and/or "drag_aero" and/or "srp" 
-
-#     print("Number of space_object in catalogue specified: ", len(SATCAT.Catalogue))
-#     print(f"Propagating SpaceObjects and saving state vectors every {settings['output_frequency']} seconds...")
-
-#     decayed_before_start = 0
-#     for space_object in SATCAT.Catalogue:
-#         if space_object.decay_date < datetime.datetime.strptime(settings["sim_start_date"], '%Y-%m-%d'):
-#             SATCAT.Catalogue.remove(space_object)
-#             decayed_before_start += 1
-#     print("# sats decayed before sim start date: ", decayed_before_start)
-
-#     #TODO: testing
-#     #slice SATCAT.Catalogue to select everth 1000th space object
-#     SATCAT.Catalogue = SATCAT.Catalogue[::1000]
-
-#     print("Propagating space objects in parallel...")
-
-#     iterable = [(space_object, jd_start, jd_stop, step_size, output_freq, integrator_type, force_model, sgp4_long_term) for space_object in SATCAT.Catalogue]
-#     with Pool(initializer=initialize_orekit, processes=cpu_count()) as pool:
-#         with tqdm(total=len(iterable)) as pbar:
-#             results = []
-#             for result in pool.imap_unordered(propagate_space_object, iterable):
-#                 results.append(result)
-#                 pbar.update()
-
-#     SATCAT.Catalogue = results
-#     print("Exporting results...")
-#     save_path = os.path.dirname(f'src/data/results/propagated_catalogs/{scenario_name}.pickle')
-#     os.makedirs(save_path, exist_ok=True)
-#     dump_pickle(f'src/data/results/propagated_catalogs/{scenario_name}.pickle', SATCAT)
-
-#     print(f"Simulation complete. Results saved to: {get_path(f'src/data/results/propagated_catalogs/{scenario_name}.pickle')}")
-
-###### MULTITHREADING ######
-
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 def run_parallel_sim(settings):
     SATCAT = SpaceCatalogue(settings=settings)
@@ -97,9 +47,11 @@ def run_parallel_sim(settings):
     integrator_type = str(settings["integrator_type"])
     sgp4_long_term = bool(settings["sgp4_long_term"])
     force_model = settings["force_model"]
+
+    # Initialize for the main process, but also will be initialized within each child process.
     initialize_orekit()
 
-    print("Number of space_object in catalogue specified: ", len(SATCAT.Catalogue))
+    print("Number of space_object in catalogue specified:", len(SATCAT.Catalogue))
     print(f"Propagating SpaceObjects and saving state vectors every {settings['output_frequency']} seconds...")
 
     decayed_before_start = 0
@@ -107,30 +59,27 @@ def run_parallel_sim(settings):
         if space_object.decay_date < datetime.datetime.strptime(settings["sim_start_date"], '%Y-%m-%d'):
             SATCAT.Catalogue.remove(space_object)
             decayed_before_start += 1
-    print("# sats decayed before sim start date: ", decayed_before_start)
+    print("# sats decayed before sim start date:", decayed_before_start)
 
     # Slice SATCAT.Catalogue to select every 1000th space object (for testing)
     SATCAT.Catalogue = SATCAT.Catalogue[::1000]
 
     print("Propagating space objects in parallel...")
 
-    # Function to update the progress bar
-    def update_result(result):
-        results.append(result)
-        pbar.update()
-
     # Create iterable
     iterable = [(space_object, jd_start, jd_stop, step_size, output_freq, integrator_type, force_model, sgp4_long_term) for space_object in SATCAT.Catalogue]
 
+    results = []
     with tqdm(total=len(iterable)) as pbar:
-        results = []
-        # Use ThreadPoolExecutor for multithreading
-        with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
-            # Submit tasks
-            futures = [executor.submit(propagate_space_object, args) for args in iterable]
-            # Register callback to update progress bar
-            for future in futures:
-                future.add_done_callback(lambda f: update_result(f.result()))
+        def callback(result):
+            results.append(result)
+            pbar.update()
+
+        # Use ProcessPoolExecutor for multiprocessing
+        with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+            # Since the map function blocks until all results are available, we can simply use it here.
+            for result in executor.map(propagate_space_object, iterable):
+                callback(result)
 
     SATCAT.Catalogue = results
     print("Exporting results...")
